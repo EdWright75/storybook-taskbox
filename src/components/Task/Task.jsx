addcd30aaa38753284e1f4f0462921c617d8e635
Ed Wright <ewright@digitalfutures.com>
2023-04-19 17:30:22 +0100

    Ellipse for long title added

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -27,6 +27,7 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
                     value={title}
                     readOnly
                     placeholder="Input title"
+                    style={{textOverflow: `ellipsis`}}
                 />
             </label>
 
