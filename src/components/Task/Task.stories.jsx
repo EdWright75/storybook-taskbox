d435fcdeb03b9acb2130911a78521d27e6a68acb
Ed Wright <ewright@digitalfutures.com>
2023-04-19 10:44:30 +0100

    Storybook upgrade and Task added

---
+++ src/components/Task/Task.stories.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Task from './Task';
+
+export default {
+    component: Task,
+    // Note: `` cannot be used here!
+    title: 'Task',
+};
+
+const Template = args => <Task {...args} />;
+
+// Template.bind({}) is a standard JavaScript technique for making a copy of a function. 
+// We use this technique to allow each exported story to set its own properties, but use the same implementation.
+
+export const Default = Template.bind({});
+Default.args = {
+    task: {
+        id: `1`,
+        title: `Test Task`,
+        state: `TASK_INBOX`,
+    },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+    task: {
+        ...Default.args.task,
+        state: `TASK_PINNED`,
+    },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+    task: {
+        ...Default.args.task,
+        state: `TASK_ARCHIVED`,
+    },
+};
